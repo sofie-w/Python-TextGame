6ca90b1134816b778c54f4b1ba462074d9329e55
Sofie Weetink <sofie.weetink@coderclass.nl>
2022-09-19 11:03:55 +0200

    describe room
    Get current Room
    Set Current Room

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -4,9 +4,13 @@ hight = 4
 width = 4
 
 class Room:
-    def __init__(self, _room_name):
-        mana.add_room(_room_name)
+    def __init__(self, _room_name, _world):
+        #mana.add_room(self)
         self.exits = []
+        self.world = _world
+        self.name = _room_name
+        
+        #self.description = ""
 #          self.description = _uitleg
         
     def test(self):
@@ -14,28 +18,31 @@ class Room:
     
     def add_exit(self, x, y):
         if x-1 >= 0:
-            self.exits.append(mana.world[x-1][y])
+            self.exits.append(self.world.world[x-1][y].name)
             print('De kamer voor je zie je '+ self.exits[len(self.exits)-1])
         if x+1 <= hight-1:
-            self.exits.append(mana.world[x+1][y])
+            self.exits.append(self.world.world[x+1][y].name)
             print('De kamer achter je is de '+ self.exits[len(self.exits)-1])
         if y-1 >= 0:
-            self.exits.append(mana.world[x][y-1])
+            self.exits.append(self.world.world[x][y-1].name)
             print('De kamer links van je is '+ self.exits[len(self.exits)-1])
         if y+1 <= width-1:
-            self.exits.append(mana.world[x][y+1])
+            self.exits.append(self.world.world[x][y+1].name)
             print('De kamer rechts van je is '+ self.exits[len(self.exits)-1])
         
          
         
     def describe(self, room):
-        print('Je bent nu in de ' + room + '.') # Maak grammatica correct bij alle kamers!  
+        print('Je bent nu in de ' + room + '.') # Maak grammatica correct bij alle kamers!
+        
+        
         
         
         
 
 class World:
     def __init__(self, _name):
+        
         self.world = [['.','.','.','.'],
                       ['.','.','.','.'],
                       ['.','.','.','.'],
@@ -43,75 +50,107 @@ class World:
         self.name = _name
         self.rooms = []
     
-    def create_world(self):
+    def create_world(self, person):
+        #Voegt alle kamers toe
+        #Zorg dat hight * width even is als het aantal kamers
+        
+        keuken = Room('keuken', self)
+        self.add_room(keuken)
+        woonkamer = Room('woonkamer', self)
+        self.add_room(woonkamer)
+        eetkamer = Room('eetkamer', self)
+        self.add_room(eetkamer)
+        badkamer = Room('badkamer', self)
+        self.add_room(badkamer)
+        gang = Room('gang', self)
+        self.add_room(gang)
+        balkon = Room('balkon', self) #!
+        self.add_room(balkon)
+        tuin = Room('tuin', self)
+        self.add_room(tuin)
+        bibliotheek = Room('bibliotheek', self)
+        self.add_room(bibliotheek)
+        slaapkamer = Room('slaapkamer', self)
+        self.add_room(slaapkamer)
+        hobbykamer = Room('hobbykamer', self)
+        self.add_room(hobbykamer)
+        fitnessruimte = Room('fitnessruimte', self)
+        self.add_room(fitnessruimte)
+        studeerkamer = Room('studeerkamer', self)
+        self.add_room(studeerkamer)
+        serre = Room('serre', self)
+        self.add_room(serre)
+        garage = Room('garage', self)
+        self.add_room(garage)
+        kelder = Room('kelder', self) #!
+        self.add_room(kelder)
+        zolder = Room('zolder', self) #!
+        self.add_room(zolder)
+        #keuken = Room('keuken', '')
+        #self.add_room(keuken)
         for i in range(hight):
             for j in range(width):
                 room = random.choice(self.rooms)
+                #print(room, "##")
                 self.world[i][j] = room
                 self.rooms.remove(room)
                 
-        
-        mana.print_world()
-        
-        #mana.first_room()
-    def print_world(self):
-        for i in range(hight):
-            print(self.world[i])
-            print(' ')
+        self.first_room(person)
+                
+                
         
         
     def add_room(self, _new_room):
         self.rooms.append(_new_room)
         
-    def first_room(self):
+        
+        
+    def first_room(self, person):
         x = random.randint(0, width-1)
 #         x = 3
         y = random.randint(0, hight-1)
         
         #stop de player in deze kamer eerst:
         firstroom = self.world[x][y]
-        #keuken = Room('keuken')
-        #woonkamer = Room('woonkamer')
-        #eetkamer = Room('eetkamer')
-        #badkamer = Room('badkamer')
-        #gang = Room('gang')
-        #balkon = Room('balkon')
-        #tuin = Room('tuin')
-        #bibliotheek = Room('bibliotheek')
-        #slaapkamer = Room('slaapkamer')
+        firstroom.describe(firstroom.name)
+        firstroom.add_exit(x,y)
+        person.set_current_room(firstroom)
+        room = person.get_current_room(self, x,y)
+        print(room)
         
-        #keuken.add_exit(x,y)
         
-        current = Room("Currrent")
-        print('Je begint in de '+ firstroom + '.')
-        #current.describe(firstroom)
-        current.add_exit(x,y)
         
 class Player:
-    def __init__(self, _name):
+    def __init__(self):
         self.inventory = []
-        self.name =  _name
+        self.name =  input("Hoe heet je: ")
         print("Hoi "+ self.name + '!')
     
     def goto_room(self, name):
         print("hoi")
         
     def set_current_room(self, room):
-        print("hoi")
+        current_room = room
+        
     
-    def get_current_room(self, x, y):
-        print("hoi")
+    def get_current_room(self, world, x, y):
+        room = world.world[x][y]
+        return(room)
     
     def pick_up(self, item):
         print("hoi")
         
+        
 class Controller:
     def __init__(self):
-        print('play')
+        self.play_game()
         
         
-    def play_game():
-        print("hoi")
+    def play_game(self):
+        person = Player()
+        mana = World("Mana") 
+        mana.create_world(person)
+        
         
         
         
@@ -121,29 +160,6 @@ class Item:
         self.item_type = _itemtype
         
         
-       
-#Maakt de wereld
-mana = World("Mana")       
-    
-    
-#Voegt alle kamers toe
-#Zorg dat hight * width even is als het aantal kamers
-keuken = Room('keuken')
-woonkamer = Room('woonkamer')
-eetkamer = Room('eetkamer')
-badkamer = Room('badkamer')
-gang = Room('gang')
-balkon = Room('balkon')
-tuin = Room('tuin')
-bibliotheek = Room('bibliotheek')
-slaapkamer = Room('slaapkamer')
-hobbykamer = Room('hobbykamer')
-fitnessruimte = Room('fitnessruimte')
-studeerkamer = Room('studeerkamer')
-serre = Room('serre')
-garage = Room('garage')
-kelder = Room('kelder')
-zolder = Room('zolder')
 
 #Items in het spel
 appel = Item('Appel', 'eten')
@@ -152,11 +168,5 @@ zwaard = Item('Zwaard', 'wapen')
 boog = Item('Pijl en Boog', 'wapen')
 sleutel_goud = Item('Gouden Sleutel', 'sleutel')
 sleutel_zilver = Item('Zilvere Sleutel', 'sleutel')
-       
 
-mana.create_world()
-
-name = input("Hey, hoe wil je in dit spel heten: ")
-user = Player(name)
-
-mana.first_room()
+speel = Controller()
