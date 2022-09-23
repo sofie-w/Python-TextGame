b6b98953d67e5aac924f4ba614e6dacad05c1bcf
Sofie Weetink <sofie.weetink@coderclass.nl>
2022-09-23 13:43:58 +0200

    Random items in de kamers
    Elke kamer krijgt een random item
    Optie: kamer bekijken

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,17 +1,19 @@
 import random
 
-hight = 4
-width = 4
-
 class Room:
     def __init__(self, _room_name, _world, _item):
         self.exits = []
         self.items = []
-        self.items.append(_item)
+        #self.items.append(_item)
         self.world = _world
         self.name = _room_name
         self.x = 0
         self.y = 0
+        self.add_item(_item)
+        
+    def add_item(self, item):
+        self.items.append(item)
+        
 
 
     def add_exit(self):
@@ -66,7 +68,7 @@ class Room:
 
 
 
-class World:
+class World: #Maakt een random wereld
     def __init__(self, _name):
 
         self.world = [['.','.','.','.'],
@@ -74,44 +76,74 @@ class World:
                       ['.','.','.','.'],
                       ['.','.','.','.']]
         self.name = _name
+        self.poss_items = []
         self.rooms = []
 
     def create_world(self, person):
+        
+        for a in range(6):
+            self.poss_items.append(appel)
+        for b in range(6):
+            self.poss_items.append(brood)
+        self.poss_items.append(zwaard)
+        self.poss_items.append(boog)
+        self.poss_items.append(sleutel_goud)
+        self.poss_items.append(sleutel_zilver)
+        
+        
+        
+        
         #Voegt alle kamers toe
         #Zorg dat hight * width even is als het aantal kamers
-
-        keuken = Room('keuken', self, appel)
+        item = self.random_item()
+        keuken = Room('keuken', self, item)
         self.add_room(keuken)
-        woonkamer = Room('woonkamer', self, appel)
+        item = self.random_item()
+        woonkamer = Room('woonkamer', self, item)
         self.add_room(woonkamer)
-        eetkamer = Room('eetkamer', self, sleutel_goud)
+        item = self.random_item()
+        eetkamer = Room('eetkamer', self, item)
         self.add_room(eetkamer)
-        badkamer = Room('badkamer', self, zwaard)
+        item = self.random_item()
+        badkamer = Room('badkamer', self, item)
         self.add_room(badkamer)
-        gang = Room('gang', self, appel)
+        item = self.random_item()
+        gang = Room('gang', self, item)
         self.add_room(gang)
-        balkon = Room('balkon', self, appel) #!
+        item = self.random_item()
+        balkon = Room('balkon', self, item) #!
         self.add_room(balkon)
-        tuin = Room('tuin', self, sleutel_goud)
+        item = self.random_item()
+        tuin = Room('tuin', self, item)
         self.add_room(tuin)
-        bibliotheek = Room('bibliotheek', self, appel)
+        item = self.random_item()
+        bibliotheek = Room('bibliotheek', self, item)
         self.add_room(bibliotheek)
-        slaapkamer = Room('slaapkamer', self, boog)
+        item = self.random_item()
+        slaapkamer = Room('slaapkamer', self, item)
         self.add_room(slaapkamer)
-        hobbykamer = Room('hobbykamer', self, brood)
+        item = self.random_item()
+        hobbykamer = Room('hobbykamer', self, item)
         self.add_room(hobbykamer)
-        fitnessruimte = Room('fitnessruimte', self, brood)
+        item = self.random_item()
+        fitnessruimte = Room('fitnessruimte', self, item)
         self.add_room(fitnessruimte)
-        studeerkamer = Room('studeerkamer', self, brood)
+        item = self.random_item()
+        studeerkamer = Room('studeerkamer', self, item)
         self.add_room(studeerkamer)
-        serre = Room('serre', self, sleutel_zilver)
+        item = self.random_item()
+        serre = Room('serre', self, item)
         self.add_room(serre)
-        garage = Room('garage', self, brood)
+        item = self.random_item()
+        garage = Room('garage', self, item)
         self.add_room(garage)
-        kelder = Room('kelder', self, brood) #!
+        item = self.random_item()
+        kelder = Room('kelder', self, item) #!
         self.add_room(kelder)
-        zolder = Room('zolder', self, sleutel_goud) #!
+        item = self.random_item()
+        zolder = Room('zolder', self, item) #!
         self.add_room(zolder)
+        
 
         for i in range(hight):
             for j in range(width):
@@ -122,29 +154,27 @@ class World:
                 self.rooms.remove(room)
 
         self.first_room(person)
-
+        
+    def random_item(self):
+        item = random.choice(self.poss_items)
+        self.poss_items.remove(item)
+        return(item)
 
 
     def add_room(self, _new_room):
         self.rooms.append(_new_room)
 
 
-
     def first_room(self, person):
         x = random.randint(0, width-1)
         y = random.randint(0, hight-1)
 
-        #stop de player in deze kamer eerst:
         firstroom = self.world[x][y]
-        #firstroom.describe()
-        #firstroom.add_exit(x,y)
         person.set_current_room(firstroom)
-        #room = person.get_current_room(self, x,y)
-        #print(room)
 
 
 
-class Player:
+class Player: #Lopen en de inventory
     def __init__(self):
         self.inventory = []
         self.name =  input("Hoe heet je: ")
@@ -158,19 +188,19 @@ class Player:
             lopen = input("Waar wil je heen(W/A/S/D): ")
             if lopen == 'W' and self.current_room.exits[0] != 'Muur':
                 self.set_current_room(self.current_room.exits[0])
-                #self.current_room = self.current_room.exits[0]
+                self.kijken_lopen()
                 door = 'nee'
             elif lopen == 'A' and self.current_room.exits[1] != 'Muur':
                 self.set_current_room(self.current_room.exits[1])
-                #self.current_room = self.current_room.exits[1]
+                self.kijken_lopen()
                 door = 'nee'
             elif lopen == 'S' and self.current_room.exits[2] != 'Muur':
                 self.set_current_room(self.current_room.exits[2])
-                #self.current_room = self.current_room.exits[2]
+                self.kijken_lopen()
                 door = 'nee'
             elif lopen == 'D' and self.current_room.exits[3] != 'Muur':
                 self.set_current_room(self.current_room.exits[3])
-                #self.current_room = self.current_room.exits[3]
+                self.kijken_lopen()
                 door = 'nee'
             else:
                 print("Je hebt een verkeerde weg gekozen of iets verkeerds ingevuld. \nProbeer het opnieuw.")
@@ -178,7 +208,10 @@ class Player:
 
     def set_current_room(self, room):
         self.current_room = room
-        print('Wil je in deze kamer voor spullen kijken(K) of gewoon verder lopen(L): ')
+        
+                
+    def kijken_lopen(self):
+        print('Wil je in deze kamer voor spullen kijken(K) of verder lopen(L): ')
         door = input('(K/L): ')
         if door == 'K':
             item = self.current_room.describe()
@@ -205,7 +238,7 @@ class Player:
 
 
 
-class Controller:
+class Controller: # Begint het spel en laat het menu zien
     def __init__(self):
         self.play_game()
 
@@ -217,14 +250,16 @@ class Controller:
         while keuze != 'S':
             clear_screen()
             print('Je bent nu in de ' + person.current_room.name)
-            print('\nLopen: L \nInventory bekijken: I \nHonger bekijken: H \nStoppen: S')
-            keuze = input("Kies(L/I/H/S): ")
+            print('\nLopen: L \nInventory bekijken: I \nKamer bekijken: K \nHonger bekijken: H \nStoppen: S')
+            keuze = input("Kies(L/I/K/H/S): ")
             clear_screen()
             if keuze == 'L':
                 person.goto_room()
                 print('')
             elif keuze == 'I':
                 person.print_inv()
+            elif keuze == 'K':
+                person.kijken_lopen()
             elif keuze == 'H':
                 print('H')
             elif keuze == 'S':
@@ -240,19 +275,26 @@ class Controller:
 
 
 
-class Item:
+class Item: #Geeft de items een naam en een soort
     def __init__(self, _name, _itemtype):
         self.item_name = _name
         self.item_type = _itemtype
 
 
-def clear_screen():
+def clear_screen(): #Maakt het scherm leeg
     #os.system("cls" if os.name == "nt" else "clear")
     print('\n\n\n\n\n\n\n\n\n\n\n') #Tijdelijke oplossing
 
 
 
 #Items in het spel
+
+
+#Hoogte en Breedte van de wereld
+hight = 4
+width = 4
+
+#Maakt de Items
 appel = Item('Appel', 'eten')
 brood = Item('Brood', 'eten')
 zwaard = Item('Zwaard', 'wapen')
@@ -260,4 +302,5 @@ boog = Item('Pijl en Boog', 'wapen')
 sleutel_goud = Item('Gouden Sleutel', 'sleutel')
 sleutel_zilver = Item('Zilvere Sleutel', 'sleutel')
 
+#Start het spel
 speel = Controller()
