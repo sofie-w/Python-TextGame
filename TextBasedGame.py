3b395a6c2b20ea5887b4ce7bf97437dfed260998
Sofie Weetink <sofie.weetink@coderclass.nl>
2022-10-11 12:07:37 +0200

    Monster + Levens
    Meer verschillende monster
    Verschillende random monsters in de kamer.
    Je kan levens bekijken

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,7 +1,7 @@
 import random
 
 class Room:
-    def __init__(self, _room_name, _world, _item):
+    def __init__(self, _room_name, _world, _item, _monster):
         self.exits = []
         self.items = []
         #self.items.append(_item)
@@ -10,19 +10,21 @@ class Room:
         self.x = 0
         self.y = 0
         self.add_item(_item)
-        self.monster = self.add_Enemy()
+        self.monster = _monster
         print(self.monster)
         
     def add_item(self, item):
         self.items.append(item)
         
     def add_Enemy(self):
-        kies = random.randint(0, 1)
-        if kies == 0:
-            return(Enemy())
+        #kies = random.randint(0, 1)
+        monster = random.choice(MONSTERS)
+        return(monster)
+        #if kies == 0:
+         #   return(Enemy())
             #self.monster = Enemy()
-        else:
-            return(Enemy_Fire())
+        #else:
+         #   return(Enemy_Fire())
             #self.monster = Enemy_Fire()
         #print(self.monster)
 
@@ -58,12 +60,13 @@ class Room:
         #print(len(self.items))
         if len(self.items) >= 1:
             print('Je ziet in de ' + self.name + ' een '+ self.items[0].item_name + ' liggen. Maar er staat een ' + self.monster.name + ' voor.') # Maak grammatica correct bij alle kamers!
+            print(self.monster.life)
             vraag = input('Wil je het '+ self.monster.name +' aanvallen(y/n): ')
             door = 'a'
             
             if vraag == 'y':
                 while self.monster.life > 0 and person.life > 0 and door != 'v':
-                    door = self.monster.attack()
+                    self.monster.attack()
                     if self.monster.life > 0 and person.life > 0:
                         vraag = input('Wil je verder met aanvallen(a) of wil je vluchten(v) (a/v): ')
                         if vraag == 'a':
@@ -109,70 +112,122 @@ class World: #Maakt een random wereld
                       ['.','.','.','.']]
         self.name = _name
         self.poss_items = []
+        self.poss_Enemy = []
         self.rooms = []
 
     def create_world(self):
         
-        for a in range(5):
+        for i in range(5):
             self.poss_items.append(appel)
-        for b in range(5):
+        for i in range(5):
             self.poss_items.append(brood)
         self.poss_items.append(zwaard)
         self.poss_items.append(boog)
         self.poss_items.append(sleutel_goud)
-        for c in range(3):
+        for i in range(3):
             self.poss_items.append(sleutel_zilver)
+            
+        monster1 = Enemy()
+        monster2 = Enemy()
+        monster3 = Enemy()
+        monster4 = Enemy()
+        monster5 = Enemy()
+        monster6 = Enemy()
+        monster7 = Enemy()
+        monster8 = Enemy_Fire()
+        monster9 = Enemy_Fire()
+        monster10 = Enemy_Fire()
+        monster11 = Enemy_Water()
+        monster12 = Enemy_Water()
+        monster13 = Enemy_Water()
+        monster14 = Enemy_Plant()
+        monster15 = Enemy_Plant()
+        monster16 = Enemy_Plant()
+        
+        self.poss_Enemy.append(monster1)
+        self.poss_Enemy.append(monster2)
+        self.poss_Enemy.append(monster3)
+        self.poss_Enemy.append(monster4)
+        self.poss_Enemy.append(monster5)
+        self.poss_Enemy.append(monster6)
+        self.poss_Enemy.append(monster7)
+        self.poss_Enemy.append(monster8)
+        self.poss_Enemy.append(monster9)
+        self.poss_Enemy.append(monster10)
+        self.poss_Enemy.append(monster11)
+        self.poss_Enemy.append(monster12)
+        self.poss_Enemy.append(monster13)
+        self.poss_Enemy.append(monster14)
+        self.poss_Enemy.append(monster15)
+        self.poss_Enemy.append(monster16)
         
         
         #Voegt alle kamers toe
         #Zorg dat hight * width even is als het aantal kamers
         item = self.random_item()
-        keuken = Room('keuken', self, item)
+        monster = self.random_enemy()
+        keuken = Room('keuken', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        woonkamer = Room('woonkamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        eetkamer = Room('eetkamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        badkamer = Room('badkamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        gang = Room('gang', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        balkon = Room('balkon', self, item, monster) #!
+        item = self.random_item()
+        monster = self.random_enemy()
+        tuin = Room('tuin', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        bibliotheek = Room('bibliotheek', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        slaapkamer = Room('slaapkamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        hobbykamer = Room('hobbykamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        fitnessruimte = Room('fitnessruimte', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        studeerkamer = Room('studeerkamer', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        serre = Room('serre', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        garage = Room('garage', self, item, monster)
+        item = self.random_item()
+        monster = self.random_enemy()
+        kelder = Room('kelder', self, item, monster) #!
+        item = self.random_item()
+        monster = self.random_enemy()
+        zolder = Room('zolder', self, item, monster) #!
+        
         self.add_room(keuken)
-        item = self.random_item()
-        woonkamer = Room('woonkamer', self, item)
         self.add_room(woonkamer)
-        item = self.random_item()
-        eetkamer = Room('eetkamer', self, item)
         self.add_room(eetkamer)
-        item = self.random_item()
-        badkamer = Room('badkamer', self, item)
         self.add_room(badkamer)
-        item = self.random_item()
-        gang = Room('gang', self, item)
         self.add_room(gang)
-        item = self.random_item()
-        balkon = Room('balkon', self, item) #!
         self.add_room(balkon)
-        item = self.random_item()
-        tuin = Room('tuin', self, item)
         self.add_room(tuin)
-        item = self.random_item()
-        bibliotheek = Room('bibliotheek', self, item)
         self.add_room(bibliotheek)
-        item = self.random_item()
-        slaapkamer = Room('slaapkamer', self, item)
         self.add_room(slaapkamer)
-        item = self.random_item()
-        hobbykamer = Room('hobbykamer', self, item)
         self.add_room(hobbykamer)
-        item = self.random_item()
-        fitnessruimte = Room('fitnessruimte', self, item)
         self.add_room(fitnessruimte)
-        item = self.random_item()
-        studeerkamer = Room('studeerkamer', self, item)
         self.add_room(studeerkamer)
-        item = self.random_item()
-        serre = Room('serre', self, item)
-        self.add_room(serre)
-        item = self.random_item()
-        garage = Room('garage', self, item)
+        self.add_room(serre)        
         self.add_room(garage)
-        item = self.random_item()
-        kelder = Room('kelder', self, item) #!
         self.add_room(kelder)
-        item = self.random_item()
-        zolder = Room('zolder', self, item) #!
         self.add_room(zolder)
         
 
@@ -184,19 +239,24 @@ class World: #Maakt een random wereld
                 room.y = j
                 self.rooms.remove(room)
 
-        self.first_room(person)
+        self.first_room()
         
     def random_item(self):
         item = random.choice(self.poss_items)
         self.poss_items.remove(item)
         return(item)
+    
+    def random_enemy(self):
+        enemy = random.choice(self.poss_Enemy)
+        self.poss_Enemy.remove(enemy)
+        return(enemy)
 
 
     def add_room(self, _new_room):
         self.rooms.append(_new_room)
 
 
-    def first_room(self, person):
+    def first_room(self):
         x = random.randint(0, width-1)
         y = random.randint(0, hight-1)
 
@@ -209,7 +269,7 @@ class Player: #Lopen en de inventory
     def __init__(self):
         self.inventory = []
         self.current_room = 'I'
-        self.life = 5
+        self.life = LEVENS_SPELER
         self.name =  input("Hoe heet je: ")
         print("Hoi "+ self.name + '!')
         
@@ -244,13 +304,10 @@ class Player: #Lopen en de inventory
         
                 
     def kijken(self):
-        #print('Wil je in deze kamer voor spullen kijken(K) of verder lopen(L): ')
-        #door = input('(K/L): ')
-        #if door == 'K':
         item = self.current_room.describe()
         if item == 'S':
             return('S')
-        elif item != 0:
+        else: #item != 0
             self.pick_up(item)
             return('K')
 
@@ -261,7 +318,6 @@ class Player: #Lopen en de inventory
 
     def pick_up(self, item):
         self.inventory.append(item)
-        #self.print_inv()
 
     def print_inv(self):
         print('Appels: '+ str(self.inventory.count(appel)))
@@ -270,6 +326,10 @@ class Player: #Lopen en de inventory
         print('Pijl en Boog: '+ str(self.inventory.count(boog)))
         print('Zilvere sleutels: '+ str(self.inventory.count(sleutel_zilver)))
         print('Goude sleutels: '+ str(self.inventory.count(sleutel_goud)))
+        enter()
+        
+    def print_life(self):
+        print('Je hebt nog ' + str(self.life) + ' levens!')
         enter()
 
 
@@ -296,14 +356,14 @@ class Controller: # Begint het spel en laat het menu zien
             elif self.keuze == 'K':
                 self.keuze = person.kijken()
             elif self.keuze == 'H':
-                print('H')
+                person.print_life()
             elif self.keuze == 'S':
                 print('Doei doei')
             else:
                 print('Je hebt iets verkeerd getypt, probeer het opnieuw.')
             
             if person.inventory.count(sleutel_goud) == 3: #Als je 3 sleutels hebt dan win je
-                keuze = 'S'
+                self.keuze = 'S'
                 print("Je hebt de goude sleutel gevonden! Je hebt gewonnen")
                     
 
@@ -319,29 +379,30 @@ class Item: #Geeft de items een naam en een soort
 
 class Enemy:
     def __init__(self):
-        self.life = 20
-        self.max_schade = 3
+        self.life = LEVENS_MONSTER
+        self.max_schade = DAMAGE_MONSTER
         self.name = 'Monster'
-        #self.name = n
 
     def attack(self):
         #print("Ouch!")
+        self.attacked()
+        
+        schade_person = random.randint(0,self.max_schade)
+        print('Het monster valt je aan hij haalt '+ str(schade_person) + ' levens van je af.')
+        person.life -= schade_person
+        #self.attacked()
+        print('Monster: '+ str(self.life) + ' - Jij: ' + str(person.life))
+        #print('Je hebt er nog '+ str(person.life) + ' over.')
+        #print("Je valt het monster aan. Hij heeft nog " + str(self.life) + " levens over")
+        
+    def attacked(self):
         wapen = self.wapen()
         #if wapen == zwaard:
         if wapen == 0:
             self.life -= 3
         else:
             self.life -= wapen.damage
-        #elif wapen == boog:
-            #self.life -= 5
-        #else:
-            #self.life -= 3
-        print('Monster heeft nog '+ str(self.life) + ' levens over.')
-        schade_person = random.randint(0,self.max_schade)
-        print('Het monster valt je aan hij haalt '+ str(schade_person) + ' levens van je af.')
-        person.life -= schade_person
-        print('Je hebt er nog '+ str(person.life) + ' over.')
-        #print("Je valt het monster aan. Hij heeft nog " + str(self.life) + " levens over")
+        
             
     def wapen(self):
         if zwaard in person.inventory:
@@ -354,11 +415,20 @@ class Enemy:
                 if keuze == 'y':
                     print('Oke laten we aan vallen met de pijl en boog')
                     return(boog)
+                else:
+                    print('Je gaat je vuisten gebruiken')
+                    return(0)
+            else:
+                print('Je gaat je vuisten gebruiken')
+                return(0)
         elif boog in person.inventory:
             keuze = input('Wil je de pijl en boog gebruiken om aan te vallen(y/n): ')
             if keuze == 'y':
                 print('Oke laten we aan vallen met de pijl en boog')
                 return(boog)
+            else:
+                print('Je gaat je vuisten gebruiken')
+                return(0)
         else:
             print('Je gaat je vuisten gebruiken')
             return(0)
@@ -377,13 +447,54 @@ class Enemy:
 class Enemy_Fire(Enemy):
     def __init__(self):
         Enemy.__init__(self)
-        self.life = 30
-        self.max_schade = 5
+        self.life = LEVENS_VUURMONSTER
+        self.max_schade = DAMAGE_VUURMONSTER
         self.name = 'Vuur Monster'
+        
     def attack(self):
         if random.randint(0, 3) == 1:
             print('Het monster spuwt vuur naar je toe, het kost je 6 levens.')
-            schade_person = 6
+            #schade_person = 6
+            person.life -= 6
+            Enemy.attacked(self)
+            print('Monster: '+ str(self.life) + ' - Jij: ' + str(person.life))
+        
+        else:
+            Enemy.attack(self)
+            
+class Enemy_Water(Enemy):
+    def __init__(self):
+        #super.__init__
+        Enemy.__init__(self)
+        self.life = LEVENS_VUURMONSTER
+        self.max_schade = DAMAGE_VUURMONSTER
+        self.name = 'Water Monster'
+        
+    def attack(self):
+        if random.randint(0, 3) == 1:
+            print('Het monster maakt een waterkolk om je heen dit kost je 6 levens.')
+            #schade_person = 6
+            person.life -= 6
+            Enemy.attacked(self)
+            print('Monster: '+ str(self.life) + ' - Jij: ' + str(person.life))
+        
+        else:
+            Enemy.attack(self)
+            
+class Enemy_Plant(Enemy):
+    def __init__(self):
+        Enemy.__init__(self)
+        self.life = LEVENS_VUURMONSTER
+        self.max_schade = DAMAGE_VUURMONSTER
+        self.name = 'Plant Monster'
+        
+    def attack(self):
+        if random.randint(0, 3) == 1:
+            print('Het monster geeft je een klap met een tak dit kost je 6 levens.')
+            #schade_person = 6
+            person.life -= 6
+            Enemy.attacked(self)
+            print('Monster: '+ str(self.life) + ' - Jij: ' + str(person.life))
         
         else:
             Enemy.attack(self)
@@ -402,10 +513,11 @@ def enter():
 hight = 4
 width = 4
 LEVENS_SPELER = 20
-LEVENS_MONSTER = 20
-LEVENS_VUURMONSTER = 25
+LEVENS_MONSTER = 10
+LEVENS_VUURMONSTER = 15
 DAMAGE_MONSTER = 4
 DAMAGE_VUURMONSTER = 6
+MONSTERS = [Enemy_Plant(), Enemy_Water(), Enemy_Fire(), Enemy()]
 
 #Maakt de Items
 appel = Item('Appel', 'eten', 0)
@@ -423,7 +535,8 @@ speel.play_game()
 
 
 #Ideeën
-# - Meerdere Enemys verschilende krachten
+# - Meerdere Enemys verschilende krachten -
+#		Normaal, Vuur, Water, Plant
 # - Levens Potion
 # - 
 
